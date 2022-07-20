1d0f2866c7b5cce246781efecbb860c07c1d2f9a
vaibhav mali <67496777+vaibhavmali907@users.noreply.github.com>
2022-07-20 13:37:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pandas as pd
-import numpy as np
 import pickle
 
 
 # data visualization library
 import matplotlib
-import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
+
+# Model evaluation
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 sns.set(context='notebook', style='darkgrid', palette='colorblind', font='sans-serif', font_scale=1, rc=None)
 matplotlib.rcParams['figure.figsize'] =[8,8]
@@ -115,16 +119,96 @@ X_train = sc.fit_transform(X_train)
 
 X_test =  sc.transform(X_test)
 
+from sklearn.linear_model import LogisticRegression
+l1 = LogisticRegression()
+l1.fit(X_train,y_train)
+
+l1_predict = l1.predict(X_test)
+
+cm = confusion_matrix(y_test, l1_predict)
+print(cm)
+
+print(classification_report(y_test,l1_predict))
+
+acc_logreg = accuracy_score(y_test, l1_predict)
+
+
+from sklearn.tree import DecisionTreeClassifier
+d1 = DecisionTreeClassifier()
+d1.fit(X_train,y_train)
+
+d1_predict = d1.predict(X_test)
+
+cm = confusion_matrix(y_test, d1_predict)
+print(cm)
+
+print(classification_report(y_test,d1_predict))
+
+acc_decision_tree = accuracy_score(y_test, d1_predict)
+
+
+from sklearn.ensemble import RandomForestClassifier
+r1 = RandomForestClassifier()
+r1.fit(X_train,y_train)
+
+r1_predict = r1.predict(X_test)
+
+cm = confusion_matrix(y_test, r1_predict)
+print(cm)
+
+print(classification_report(y_test,r1_predict))
+
+acc_random_forest = accuracy_score(y_test, r1_predict)
+
+from sklearn.neighbors import KNeighborsClassifier
+k1 = KNeighborsClassifier(n_neighbors=10)
+k1.fit(X_train,y_train)
+
+k1_predict = k1.predict(X_test)
+
+cm = confusion_matrix(y_test,k1_predict)
+print(cm)
+
+print(classification_report(y_test,k1_predict))
+
+acc_knn = accuracy_score(y_test, k1_predict)
+
+
 
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-
 model = SVC()
 model.fit(X_train, y_train)
 
 covid_model = model.predict(X_test)
 
-accuracy_score(y_test, covid_model)
+cm = confusion_matrix(y_test, covid_model)
+print(cm)
+
+print(classification_report(y_test,covid_model))
+
+acc_svc = accuracy_score(y_test, covid_model)
+
+
+
+models = pd.DataFrame({
+    'Model': ['Logistic Regression','Decision Tree','Random Forest','KNN','Support Vector Machines'],
+    'Score': [acc_logreg, acc_decision_tree, acc_random_forest, acc_knn , acc_svc]})
+models.sort_values(by='Score', ascending=False)
+
+plt.rcParams['figure.figsize']=15,6
+sns.set_style("darkgrid")
+ax = sns.barplot(x=models.Model, y=models.Score, palette = "rocket", saturation =1.5)
+plt.xlabel("Classifier Models", fontsize = 20 )
+plt.ylabel("% of Accuracy", fontsize = 20)
+plt.title("Accuracy of different Classifier Models", fontsize = 20)
+plt.xticks(fontsize = 12, horizontalalignment = 'center', rotation = 8)
+plt.yticks(fontsize = 13)
+for p in ax.patches:
+    width, height = p.get_width(), p.get_height()
+    x, y = p.get_xy()
+    ax.annotate(f'{height:.2%}', (x + width/2, y + height*1.02), ha='center', fontsize = 'x-large')
+plt.show()
+
 
 import pickle
 
@@ -138,4 +222,3 @@ df = pd.read_csv('test.csv')
 output = predict(df)
 
 print(output)
-
